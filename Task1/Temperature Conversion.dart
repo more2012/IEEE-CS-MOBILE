f1c3531077eacd752c60f6f55eaca4dcf3ac46b4
Mohamed Wael <127055244+more2012@users.noreply.github.com>
2025-02-06 22:47:55 +0200

    Create Temperature Conversion.dart

---
+++ Task1/Temperature Conversion.dart
@@ -0,0 +1,7 @@
+void main() {
+  double celsius = 25;
+  double fahrenheit = celsius * 9 / 5 + 32;
+
+  print("Temperature in Celsius: $celsius");
+  print("Temperature in Fahrenheit: $fahrenheit");
+}
