d13851cc06546a07c50e1d497e7e9dbe5e8b9419
Mohamed Wael <127055244+more2012@users.noreply.github.com>
2025-02-08 23:31:40 +0200

    Update divisors of number.dart

--- Task2/divisors of number.dart
+++ Task2/divisors of number.dart
@@ -1,1 +1,14 @@
+import 'dart:io';
 
+void main() {
+  stdout.write("Enter a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Divisors of $number: ");
+  for (int i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      stdout.write("$i ");
+    }
+  }
+  print("");
+}
