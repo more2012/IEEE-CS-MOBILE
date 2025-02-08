7c808c84c1aec5a5b933c0ab0453436d5bab2c46
Mohamed Wael <127055244+more2012@users.noreply.github.com>
2025-02-08 23:35:05 +0200

    Create EvenNumberForm1to20.dart

---
+++ Task2/EvenNumberForm1to20.dart
@@ -0,0 +1,15 @@
+void main() {
+  for (int i = 2; i <= 20; i += 2) {
+    print(i);
+  }
+}
+/*
+void main() {
+  for (int i = 1; i <= 20; i++) 
+  {
+    if(i%2==0) {
+      print(i);
+    }
+  }
+}
+*/
