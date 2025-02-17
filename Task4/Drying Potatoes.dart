93be67c11d270676903e3cc081742efebaeafd65
Mohamed Wael <127055244+more2012@users.noreply.github.com>
2025-02-17 20:16:00 +0200

    Create Drying Potatoes.dart

---
+++ Task4/Drying Potatoes.dart
@@ -0,0 +1,10 @@
+int potatoes(int p0, int w0, int p1) {
+  double dryMatter= w0*(100-p0) /100; 
+  int w1= (dryMatter*100 /(100-p1)).truncate(); 
+  return w1;
+}
+void main() {
+  print(potatoes(99, 100, 98)); // 50
+  print(potatoes(90, 200, 80)); // 100
+  print(potatoes(95, 100, 90)); // 50
+}
